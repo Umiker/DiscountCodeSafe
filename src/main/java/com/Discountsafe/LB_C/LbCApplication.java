7ee7c24c6d36da4799e0036d090bf1fbeb3b9605
Dino <dino.neptun@gmail.com>
2019-09-10 09:56:06 +0200

    first commit

---
+++ src/main/java/com/Discountsafe/LB_C/LbCApplication.java
@@ -0,0 +1,14 @@
+package com.Discountsafe.LB_C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbCApplication.class, args);
+		//hello
+	}
+
+}
---
+++ src/test/java/com/Discountsafe/LB_C/LbCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Discountsafe.LB_C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
