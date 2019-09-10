be08d37716fe2ef23eb8b2bac9c2cba993e00c3e
Dino <dino.neptun@gmail.com>
2019-09-10 15:28:50 +0200

    security configuration

--- src/main/java/com/Discountsafe/LB_C/LbCApplication.java
+++ src/main/java/com/Discountsafe/LB_C/LbCApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//repository
 public class LbCApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LbCApplication.class, args);
-		//hello
 	}
 
 }
